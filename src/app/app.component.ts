ce47eda8cf111ffbd41f01d4065976a9d95b73a8
Naveed Aheer <naveedaheerpc@gmail.com>
2023-09-07 17:55:55 +0500

    Implemented i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-online-store';
+
+  constructor(private titleService: Title) {
+    this.titleService.setTitle($localize`${this.title}`);
+  }
 }
