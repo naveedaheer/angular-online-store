72d602d15938de48cf55a35ba076c7d56574ae7b
Naveed Aheer <naveedaheerpc@gmail.com>
2023-09-08 06:17:47 +0500

    Initialized login component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [
+    LoginComponent
+  ],
+  imports: [CommonModule, AuthRoutingModule],
   providers: [],
 })
 export class AuthModule {}
