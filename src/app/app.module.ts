d5bb586cbde184bef56362e8120874805f1dd5a8
Naveed Aheer <naveedaheerpc@gmail.com>
2023-09-09 11:55:11 +0500

    imports ngrx config in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { CoreModule } from './core/core.module';
 import { HttpClientModule } from '@angular/common/http';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { AuthEffects } from './auth/state/auth.effects';
+import { appReducer } from './store/app.state';
 
 @NgModule({
   declarations: [
@@ -24,6 +28,8 @@ import { HttpClientModule } from '@angular/common/http';
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
     }),
+    EffectsModule.forRoot([AuthEffects]),
+    StoreModule.forRoot(appReducer),
   ],
   providers: [],
   bootstrap: [AppComponent]
